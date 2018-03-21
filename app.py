ea111547b7de268e425af635f7a6ab046ff69a64
Craig Boman <craig.boman@gmail.com>
2018-03-21 16:40:55 -0400

    testing authentication

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sys, psycopg2, datetime, configparser, re
+import sys, psycopg2, datetime, configparser, re, requires_auth
 from flask import Flask, json
 from flask_restful import Resource, Api
 
@@ -26,6 +26,86 @@ config.read('app.ini')
 app = Flask(__name__)
 api = Api(app)
 
+
+#start auth0
+def get_token_auth_header():
+    """Obtains the access token from the Authorization Header
+    """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+def requires_auth(f):
+    """Determines if the access token is valid
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer="https://"+AUTH0_DOMAIN+"/"
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError({"code": "token_expired",
+                                "description": "token is expired"}, 401)
+            except jwt.JWTClaimsError:
+                raise AuthError({"code": "invalid_claims",
+                                "description":
+                                    "incorrect claims,"
+                                    "please check the audience and issuer"}, 401)
+            except Exception:
+                raise AuthError({"code": "invalid_header",
+                                "description":
+                                    "Unable to parse authentication"
+                                    " token."}, 400)
+
+            _app_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
+        raise AuthError({"code": "invalid_header",
+                        "description": "Unable to find appropriate key"}, 400)
+    return decorated
+#end auth0
+
+
 class GetItemInfo(Resource):
 	def get(self, barcode):
 		# we may want to consider moving the connection to the main application,
@@ -37,7 +117,7 @@ class GetItemInfo(Resource):
 		barcode = b[-1] #only need this whilst testing on port 5001; stores last list item, in this case a barcode
 		if len(barcode) != 14:
 			return 'barcode {} is not 14 characters long'.format(barcode)
-		#length checker seems to freek out if you put a / in the middle of barcode
+		#length checker seems to freak out if you put a / in the middle of barcode
 
 
 		try:
